6525b1b85c9246defed65de32afae375561426c5
Patrik Noah Šikač <psikac@foi.hr>
2020-11-05 20:54:35 +0100

    Izrađen dizajn praznog ekrana
    Stvarni kreator: Marija Arki

--- planerify/lib/main.dart
+++ planerify/lib/main.dart
@@ -4,29 +4,39 @@ void main() {
   runApp(MyApp());
 }
 
+
+
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      title: 'Welcome to Flutter',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: PreferredSize(
+            preferredSize: Size(double.infinity, 90),
+            child: AppBar(
+              flexibleSpace: Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 20),
+                        Text("Planerify",
+                            style: TextStyle(color: Colors.white,
+                                fontSize: 26)),
+                        Expanded(
+                            child: Container(
+                                alignment: Alignment.centerLeft,
+                                child: Text("Bilješke",
+                                    style: TextStyle(color: Colors.white,
+                                        fontSize: 16))))
+                      ])),
+            )),
+        body: Center(
+          child: Text('Unesite bilješku'),
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
