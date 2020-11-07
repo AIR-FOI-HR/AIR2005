f3cf5a89dd0d44a73a7c580703fc80b0b720dc73
Milan Kovacic <milan@milankovacic.com>
2020-11-07 22:00:08 +0100

    Add login screen

---
+++ planerify/lib/login.dart
@@ -0,0 +1,61 @@
+// Adapted from http://www.codeplayon.com/2020/02/simple-flutter-login-screen-ui-example/
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() => new _State();
+}
+
+class _State extends State<LoginPage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Login'),
+        ),
+        body: Padding(
+            padding: EdgeInsets.all(10),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: nameController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'E-mail',
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                  child: TextField(
+                    obscureText: true,
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                  ),
+                ),
+
+                Container(
+                    height: 50,
+                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: RaisedButton(
+                      textColor: Colors.white,
+                      color: Colors.blue,
+                      child: Text('Login'),
+                      onPressed: () {
+                        print(nameController.text);
+                        print(passwordController.text);
+                      },
+                    )),
+              ],
+            )));
+  }
+}
