94fba48962bb92c97209164e72300a63de472f92
Patrik Noah Šikač <psikac@foi.hr>
2020-12-06 13:32:29 +0100

    Napravljena povezanost sa Firestore bazom podataka
    Uspješno je napravljena veza sa Firestore bazom podataka pomoću koje se događaji unutar kalendara mogu prikazivati.

---
+++ planerify/lib/screens/add_event.dart
@@ -0,0 +1,155 @@
+//implementation based on https://github.com/lohanidamodar/flutter_calendar
+//and https://pub.dev/packages/flutter_datetime_picker
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:planerify/models/event.dart';
+import 'package:planerify/res/event_firestore_service.dart';
+
+class AddEventPage extends StatefulWidget {
+  final EventModel note;
+
+  const AddEventPage({Key key, this.note}) : super(key: key);
+
+  @override
+  _AddEventPageState createState() => _AddEventPageState();
+}
+
+class _AddEventPageState extends State<AddEventPage> {
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+  TextEditingController _title;
+  TextEditingController _description;
+  DateTime _eventDate;
+  final _formKey = GlobalKey<FormState>();
+  final _key = GlobalKey<ScaffoldState>();
+  bool processing;
+
+  @override
+  void initState() {
+    super.initState();
+    _title = TextEditingController(text: widget.note != null ? widget.note.title : "");
+    _description = TextEditingController(text:  widget.note != null ? widget.note.description : "");
+    _eventDate = DateTime.now();
+    processing = false;
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.note != null ? "Izmjeni bilješku" : "Dodaj bilješku"),
+      ),
+      key: _key,
+      body: Form(
+        key: _formKey,
+        child: Container(
+          alignment: Alignment.center,
+          child: ListView(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                child: TextFormField(
+                  controller: _title,
+                  validator: (value) =>
+                  (value.isEmpty) ? "Unesite naziv" : null,
+                  style: style,
+                  decoration: InputDecoration(
+                      labelText: "Naziv događaja",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                child: TextFormField(
+                  controller: _description,
+                  minLines: 3,
+                  maxLines: 5,
+                  validator: (value) =>
+                  (value.isEmpty) ? "Unesite opis događaja" : null,
+                  style: style,
+                  decoration: InputDecoration(
+                      labelText: "Opis događaja",
+                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
+                ),
+              ),
+              const SizedBox(height: 10.0),
+              ListTile(
+                title: Text("Datum (YYYY-MM-DD)"),
+                subtitle: Text("${_eventDate.year} - ${_eventDate.month} - ${_eventDate.day} ${_eventDate.hour}: ${_eventDate.minute}"),
+                onTap: ()async{
+                  DateTime pickedDateTime = await DatePicker.showDateTimePicker(context,
+                      showTitleActions: true,
+                      minTime: DateTime(_eventDate.year-5),
+                      maxTime: DateTime(_eventDate.year+5),
+                      currentTime: DateTime.now(),
+                      locale: LocaleType.en);
+                  if(pickedDateTime != null) {
+                    setState(() {
+                      _eventDate = pickedDateTime;
+                    });
+                  }
+                },
+              ),
+
+              SizedBox(height: 10.0),
+              processing
+                  ? Center(child: CircularProgressIndicator())
+                  : Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Material(
+                  elevation: 5.0,
+                  borderRadius: BorderRadius.circular(30.0),
+                  color: Theme.of(context).primaryColor,
+                  child: MaterialButton(
+                    onPressed: () async {
+                      if (_formKey.currentState.validate()) {
+                        setState(() {
+                          processing = true;
+                        });
+                        if(widget.note != null) {
+                          await eventDBS.updateData(widget.note.id,{
+                            "title": _title.text,
+                            "description": _description.text,
+                            "event_date": widget.note.eventDate
+                          });
+                        }
+                        else {
+                          Map<String,dynamic> newEvent = {
+                            "title": _title.text,
+                            "description": _description.text,
+                            "eventDate": _eventDate};
+                            await eventDBS.create(newEvent);
+                        }
+                        Navigator.pop(context);
+                        setState(() {
+                          processing = false;
+                        });
+                      }
+                    },
+                    child: Text(
+                      "Spremi",
+                      style: style.copyWith(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _title.dispose();
+    _description.dispose();
+    super.dispose();
+  }
+}
--- planerify/lib/screens/calendarScreen.dart
+++ planerify/lib/screens/calendarScreen.dart
@@ -1,103 +1,166 @@
+//implementation was based on project found on https://github.com/lohanidamodar/flutter_calendar
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:planerify/models/event.dart';
+import 'package:planerify/screens/view_event.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-
-// Example holidays
-final Map<DateTime, List> _holidays = {
-  DateTime(2020, 1, 1): ['New Year\'s Day'],
-  DateTime(2020, 1, 6): ['Epiphany'],
-  DateTime(2020, 2, 14): ['Valentine\'s Day'],
-  DateTime(2020, 4, 21): ['Easter Sunday'],
-  DateTime(2020, 4, 22): ['Easter Monday'],
-};
+import 'add_event.dart';
 
 
-
+//Izrada ekrana kalendara kao Stateless Widgeta
+//Kao home mu se zadaje Stateful Widget CalendarPage
+//Definira se ruta add_event koja vodi do ekrana za dodavanje novog događaja
 class CalendarScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Table Calendar Demo',
+      title: 'Planerify Calendar',
       theme: ThemeData(
         primarySwatch: Colors.cyan,
       ),
-      home: MyHomePage(title: 'Table Calendar Demo'),
+      home: CalendarPage(title: 'Planerify Calendar'),
+      routes: {
+        "add_event": (_) => AddEventPage(),
+      }
+      ,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+//CalendarPage sadrži stanje koje se zove CreateState kojime se izrađuje cijeli kalendar
+class CalendarPage extends StatefulWidget {
+  CalendarPage({Key key, this.title}) : super(key: key);
 
   final String title;
   @override
-  _MyHomepageState createState() => _MyHomepageState();
+  _CalendarPageState createState() => _CalendarPageState();
 }
+//_CalendarPageState je stanje CalendarPage klase
+//na početku se inicijaliziraju kontroler za kalendar, te mapa događaja koji se prikazuju te lista odabranih događaja
+class _CalendarPageState extends State<CalendarPage> {
+  CalendarController _calendarController;
+  Map<DateTime, List<dynamic>> _events;
+  List<dynamic> _selectedEvents;
 
-class _MyHomepageState extends State<MyHomePage> {
-  CalendarController _controller;
+
   @override
   void initState() {
-    // TODO: implement initState
-      super.initState();
-      _controller = CalendarController();
+    super.initState();
+    _calendarController = CalendarController();
+    _events = {};
+    _selectedEvents = [];
   }
+
+  //funkcija za grupiranje događaja po datumu
+  //Ulaz: Lista svih događaja
+  //Izlaz: Mapa čiji je ključ određeni datum, a vrijednost je lista događaja vezana za taj datum
+  Map<DateTime, List<dynamic>> _groupEvents(List<EventModel> events) {
+    Map<DateTime, List<dynamic>> data = {};
+    events.forEach((event) {
+      DateTime date = DateTime(event.eventDate.year, event.eventDate.month, event.eventDate.day,12);
+      if(data[date] == null)
+        data[date] = [];
+      print(event);
+      data[date].add(event);
+    });
+    return data;
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Planer"),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-              TableCalendar(
-                locale: 'en_US',
-                calendarStyle: CalendarStyle(
-                  todayColor: Colors.cyan,
-                  selectedColor: Colors.redAccent.shade200,
-                  todayStyle: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18.0,
-                    color: Colors.white
-                  ),
-                ),
-                  headerStyle: HeaderStyle(
-                    centerHeaderTitle: true,
-                    formatButtonTextStyle: TextStyle(
-                      color: Colors.white
-                    ),
-                    formatButtonDecoration: BoxDecoration(
-                      color: Colors.cyan,
-                      borderRadius: BorderRadius.circular(20.0)
-                    ),
-                    formatButtonShowsNext: false
-                  ),
-                  startingDayOfWeek: StartingDayOfWeek.monday,
-                  onDaySelected: (day, events, holidays){
-                    print(day.toIso8601String());
-                  },
-                  builders: CalendarBuilders(
-                    selectedDayBuilder: (context, date, events) =>
-                        Container(
-                          alignment: Alignment.center,
-                            margin: const EdgeInsets.all(4.0),
-                            decoration: BoxDecoration(
-                              color: Colors.red.shade300,
-                              shape: BoxShape.circle
-                            ),
-                            child:Text(date.day.toString(),
-                              style: TextStyle(
-                                color: Colors.white
-                              ),))
-                  ),
-                  calendarController: _controller)
-          ],
-        ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection("events").snapshots(),
+        builder: (context, snapshot) {
+          if(snapshot.hasData){
+            List<EventModel> allEvents = [];
+            snapshot.data.docs.forEach((element) => allEvents.add(EventModel.fromDS(element.id,element.data())));
+            if(allEvents.isNotEmpty){
+              _events = _groupEvents(allEvents);
+            }
+            else {
+              _events = {};
+              _selectedEvents = [];
+            }
+          }
+          return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                _buildTableCalendar(),
+                Expanded(child: _buildEventList()),
+              ],
+
+          );
+        }
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => Navigator.pushNamed(context, 'add_event'),
       ),
     );
   }
+
+  Widget _buildEventList() {
+    return ListView(
+      children: _selectedEvents
+          .map((event) => Container(
+        decoration: BoxDecoration(
+          border: Border.all(width: 0.8),
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        margin:
+        const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+        child: ListTile(
+          title: Text(event.title),
+          trailing: Text('${TimeOfDay.fromDateTime(event.eventDate).format(context)} '),
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (_) => EventDetailsPage(
+                      event: event,
+                    )));
+          },
+        ),
+      ))
+          .toList(),
+    );
+
+  }
+  Widget _buildTableCalendar() {
+    return TableCalendar(
+      calendarController: _calendarController,
+      events: _events,
+      startingDayOfWeek: StartingDayOfWeek.monday,
+      calendarStyle: CalendarStyle(
+        selectedColor: Colors.red[400],
+        todayColor: Colors.red[200],
+        markersColor: Colors.black87,
+        outsideDaysVisible: false,
+      ),
+      headerStyle: HeaderStyle(
+        formatButtonShowsNext: false,
+        formatButtonTextStyle:
+        TextStyle().copyWith(color: Colors.white, fontSize: 15.0),
+        formatButtonDecoration: BoxDecoration(
+          color: Colors.red[400],
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+      ),
+      onDaySelected: (day, events, holidays){
+        setState(() {
+          _selectedEvents = events;
+        });
+      },
+    );
+  }
 }
+
 
