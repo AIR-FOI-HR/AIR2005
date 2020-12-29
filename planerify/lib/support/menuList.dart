8b34d2a0ab1c6791e1acbe366e6643fe68044b96
pbanic <59288444+pbanic@users.noreply.github.com>
2020-12-29 19:39:55 +0100

    Prikaz bilješki temperatura u meniju

---
+++ planerify/lib/support/menuList.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:planerify/screens/calendarScreen.dart';
+import 'package:planerify/screens/notes.dart';
+import 'package:planerify/screens/temperatureNotes.dart';
+
+import '../models/menuOption.dart';
+
+List<MenuOption> listOfCards = <MenuOption>[];
+
+
+getInstance(int index) {
+  return listOfCards[index];
+}
+
+InitializeList()
+{
+
+  listOfCards.add(new MenuOption("Bilješke", Icons.note, Notes()));
+  listOfCards.add(new MenuOption("Kalendar", Icons.calendar_today_outlined, Calendar()));
+  listOfCards.add(new MenuOption("Bilješke o temperaturi", Icons.coronavirus_outlined, Temperature()));
+}
